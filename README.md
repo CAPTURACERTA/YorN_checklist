9e21ddc28a3196dd117f22989ec38ae4a9516277
Davi Capitano <tecapcerta@gmail.com>
2025-06-22 15:06:47 -0300

    rastreio e retorno de perguntas e parâmetros completo!
    Aqui, as funções que rastreiam e retornam as perguntas com seus parâmetros está completa.
    
    Ficarei com essa versão por hora e seguirei para outras funcionalidades.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Yes or No Automatic Checklist
 Em desenvolvimento. :D
+[Conversa em que o Gemini me auxilia](https://g.co/gemini/share/9c49c1657331)
--- yorn.py
+++ yorn.py
@@ -4,20 +4,34 @@ import re
 def main():
     with open('template.txt') as file:
         data = get_questions(''.join(file.readlines()))
+        print('-=-'*20)
+        print(data)
 
+        print('---'*20)
         cleaned_questions = []
         for question in data:
-            cleaned_questions.append(clean_questions(question))
+            if current_question := format_questions(question):
+                cleaned_questions.append(current_question) 
+        print(cleaned_questions)
+        print('-=-'*20)
 
 
 def get_questions(lines):
-    pattern = r'-*>[^;]*;'
+    pattern = r'-*>([^;]*);'
     return re.findall(pattern, lines)
 
 
-def clean_questions(question):
-    s = re.sub(r'-*>|;', '', question).strip()
-    return re.sub(r'\s+', ' ', s)
+def format_questions(question):
+    pattern = r'(?P<phrase>[^{}]*)(?:\{(?P<points>\d)?,?(?P<C>[ysn])?\})?'
+    phrase, points, coment = re.search(pattern, question, re.IGNORECASE).group('phrase','points','C')
+
+    if not phrase: return None
+    points = int(points) if points else 1
+    if not coment: coment = 'n'
+
+    phrase = re.sub('\s+', ' ', phrase)
+
+    return (phrase,points,coment)
 
 
 if __name__ == '__main__':
